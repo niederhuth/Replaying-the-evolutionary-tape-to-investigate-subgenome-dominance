9525ed4e7903d72bf5476ccb56089cb5ea832dac
Niederhuth <niederhu@msu.edu>
2019-06-04 14:41:00 -0400

    stuff

--- scripts/all_genes_downstream_methylation.py
+++ scripts/all_genes_downstream_methylation.py
@@ -1,7 +1,7 @@
+import os
 import sys
 import pandas as pd
 import pybedtools as pbt
-from os import remove
 from subprocess import call
 
 functionsfile = '../../../scripts/functions.py'
@@ -18,7 +18,8 @@ mc_type=['CG','CHG','CHH']
 updown_stream=2000
 cutoff=0
 first_feature='gene'
-filtered_output="all_downstream_filtered_allc.tmp"
+flank_bed='bed.tmp'
+filtered_output="all_genes_filtered_allc.tmp"
 output='results/all_genes_downstream_methylation.txt'
 
 #get chromosome list
@@ -26,16 +27,18 @@ chrs = list(pd.read_csv(genome_file,header=None,usecols=[0],dtype='str',sep="\t"
 chrs = list(set(chrs).difference(filter_chr))
 
 #extract downstream regions
-d_bed = pbt.bedtool.BedTool.flank(annotations,g=genome_file,l=updown_stream,r=0,s=True).saveas('bed.tmp')
+print('Making gff file of flanking regions')
+bed = pbt.BedTool(annotations)
+pbt.bedtool.BedTool.flank(bed,g=genome_file,l=updown_stream,r=0,s=True).saveas('bed.tmp')
 #correct sites where the start is greater than the end
 command="awk -v FS='\t' -v OFS='\t' '{$5=($4>$5&&$4==1?$4:$5)}; {$4=($4>$5&&$4!=1?$5:$4)} 1' bed.tmp > tmp; mv tmp bed.tmp"
 call(command, shell=True)
 
 #get promoter methylation data
 print('Getting gene methylation data')
-functions.feature_methylation(filtered_output,annotations,genome_file,output=output,
+functions.feature_methylation(filtered_output,flank_bed,genome_file,output=output,
 	mc_type=mc_type,updown_stream=updown_stream,
 	feature=first_feature,cutoff=cutoff,chrs=chrs)
 
 #remove tmp files
-remove('bed.tmp')
+os.remove('bed.tmp')
