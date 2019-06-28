062f700dc9470b08f3677fdeea5813d9dc217c56
Niederhuth <niederhu@msu.edu>
2019-06-28 11:16:16 -0400

    tired

---
+++ scripts/filter_DMR.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pandas as pd
+
+functionsfile = '../../../scripts/functions.py'
+sys.path.append(os.path.dirname(os.path.expanduser(functionsfile)))
+
+import functions
+
+#define variables
+dmr_file=sys.argv[1]+"_rms_results_collapsed.tsv"
+output=sys.argv[1]+"_filtered_rms_results_collapsed.tsv"
+min_dms=5
+min_mC_diff=0.2
+
+#filter DMRs
+functions.filter_dmr(dmr_file,output=output,min_dms=min_dms,min_mC_diff=min_mC_diff)
+
